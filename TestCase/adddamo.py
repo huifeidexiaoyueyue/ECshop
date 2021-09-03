43e935dac33093bd21695ea0cf9bc82de9188fb4
yueyue <2441991778@qq.com>
2021-09-03 20:29:44 +0800

    fgf

--- TestCase/adddamo.py
+++ TestCase/adddamo.py
@@ -13,7 +13,10 @@ class addDemo(unittest.TestCase):
     def test_something(self):
         driver = self.driver
         driver.get(self.base_url)
+        driver.find_element_by_id("kw").send_keys("51testing")
+        driver.find_element_by_id("su").click()
         sleep(5)
+        self.assertEqual("51testing_百度搜索", driver.title)
 
     def tearDown(self):
         self.driver.quit()
